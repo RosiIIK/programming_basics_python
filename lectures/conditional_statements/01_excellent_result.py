7cf9ff4a8172a9efa5d463ae8603cb058ffc005b
Rositsa Ilieva-Kitova <105305366+RosiIIK@users.noreply.github.com>
2022-05-15 11:56:07 +0300

    Create 01_excellent_result.py
    Condition of the task:
    to write console program, which to read the grade (real number), added by user and to print "Excellent!", if the grade is "5.50"or higher.

---
+++ lectures/conditional_statements/01_excellent_result.py
@@ -0,0 +1,5 @@
+# please write console program, which to read the grade (real number), added by user and to print Ëxcellent!", if the grade is "5.50"or higher.
+
+grade = float(input())
+if grade >= 5.50:
+    print("Excellent!")
