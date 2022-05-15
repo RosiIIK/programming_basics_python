64f0b828df30ebeb53829f69bcb860401dec6554
Rositsa Ilieva-Kitova <105305366+RosiIIK@users.noreply.github.com>
2022-05-15 13:19:23 +0300

    Create 04_vacation_books_list.py
    Task 4

---
+++ exercises/first_steps_in_coding/04_vacation_books_list.py
@@ -0,0 +1,7 @@
+number_of_pages = int(input())
+pages_per_hour = int(input())
+number_of_days = int(input())
+
+needed_hours = number_of_pages / pages_per_hour
+hours_per_day = needed_hours // number_of_days
+print(hours_per_day)
