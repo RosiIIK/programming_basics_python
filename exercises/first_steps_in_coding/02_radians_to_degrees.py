29bbe52a4917ee2e5522dc694764efe359501604
Rositsa Ilieva-Kitova <105305366+RosiIIK@users.noreply.github.com>
2022-05-15 13:16:22 +0300

    Create 02_radians_to_degrees.py
    Task 2: 
    to write a program, which read an angle in radians (decimal number) and transfer it to degrees. Use the formula: degrees = radians * 180 / pi

---
+++ exercises/first_steps_in_coding/02_radians_to_degrees.py
@@ -0,0 +1,9 @@
+from math import pi
+
+#We must read the input as input for radians
+radians = float(input())
+
+#we must import the number "pi"
+#We must calculate the degrees
+degrees = radians * 180 / pi
+print(degrees)
