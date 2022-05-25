e9ce770de84004db35746d318e6cd7264b1b9719
Rositsa Ilieva-Kitova <105305366+RosiIIK@users.noreply.github.com>
2022-05-25 19:25:22 +0300

    Create 05_salary.py
    Task 05

---
+++ exercises/for_loop/05_salary.py
@@ -0,0 +1,21 @@
+open_tabs = int(input())
+salary = int(input())
+
+facebook = 150
+instagram = 100
+reddit = 50
+
+for tab in range(open_tabs):
+    website = input()
+
+    if website == "Facebook":
+        salary -= facebook
+    elif website == "Instagram":
+        salary -= instagram
+    elif website == "Reddit":
+        salary -= reddit
+
+if salary <= 0:
+    print("You have lost your salary.")
+else:
+    print(salary)
