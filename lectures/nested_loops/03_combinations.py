048c674cec9ff7219f30d85d59fa59285fda3497
Rositsa Ilieva-Kitova <105305366+RosiIIK@users.noreply.github.com>
2022-06-02 15:38:09 +0300

    Create 03_combinations.py
    Task 03

---
+++ lectures/nested_loops/03_combinations.py
@@ -0,0 +1,11 @@
+n = int(input())
+# 0 -> 25
+counter = 0
+for x1 in range(n + 1):
+    for x2 in range(n + 1):
+        for x3 in range(n + 1):
+            sum = x1 + x2 + x3
+            if sum == n:
+              counter += 1
+
+print(counter)
